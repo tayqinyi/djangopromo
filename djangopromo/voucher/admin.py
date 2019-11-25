47d6bbf732a327cb838466531a040bb907b3f202
tayqinyi <37528962+tayqinyi@users.noreply.github.com>
2019-11-25 23:16:27 +0800

    Improve formatting and admin display

--- djangopromo/voucher/admin.py
+++ djangopromo/voucher/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import FixedVoucher, PercentVoucher
 
+
 # Register your models here.
+class VoucherAdmin(admin.ModelAdmin):
+    list_display = ('code', 'amount', 'uses')
+
+
 admin.site.register([FixedVoucher,
-                     PercentVoucher])
+                     PercentVoucher], VoucherAdmin)
--- djangopromo/voucher/models.py
+++ djangopromo/voucher/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from core.models import Currency
 
+
 # Create your models here.
 class BaseVoucher(models.Model):
     code = models.CharField(unique=True, max_length=8, blank=False, null=False);
@@ -15,8 +16,10 @@ class BaseVoucher(models.Model):
         if len(self.code) < 5 or len(self.code) > 8:
             raise ValidationError({'code': 'The code length should be 5-8 characters.'})
 
+
 class FixedVoucher(BaseVoucher):
     currency = models.OneToOneField(Currency, on_delete=models.DO_NOTHING)
+
 
 class PercentVoucher(BaseVoucher):
 
@@ -24,5 +27,3 @@ class PercentVoucher(BaseVoucher):
         super().clean()
         if self.quantity <=0 or self.quantity >= 100:
             raise ValidationError({'quantity': 'Percent discount should be between 0 to 100.'})
-
-
